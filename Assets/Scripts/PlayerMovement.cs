69b9c57d56cd8195fed84cad7050e0b070bad674
Berk KILIÇ <berk.kilic0638@gmail.com>
2022-10-21 18:05:59 +0300

    Movement-Camera

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, 0, zValue);
+    }
+}
